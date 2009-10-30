9e1327869092c45200ad7d39e6244a7e79d3842b
Blythe Dunham <blythedunham@snowgiraffe-super-funtime.local>
2009-10-29 19:38:04 -0700

    remove the mongo connection test that is failing

--- test/performance/trackers_test.rb
+++ test/performance/trackers_test.rb
@@ -2,10 +2,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 require 'performance_test_help'
 
 class MongoTest < ActionController::PerformanceTest
-  def test_mongo_connection
-    db = Mongo::Connection.new('localhost').db('sample-db')
-  end
-
   PERF_ACTIONS = TrackersController::TRACKED_ACTIONS +
     %w( mongo_filterless mongomapper_filterless ) unless defined?( PERF_ACTIONS )
 
