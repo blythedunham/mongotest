cfa7fed6522fa8efd4db031e719c165b45fdf62c
Blythe Dunham <blythedunham@snowgiraffe-super-funtime.local>
2009-11-05 21:03:21 -0800

    Update request context to automatically append missing request delegated methods at runtime

--- vendor/plugins/stalkerazzi/lib/stalkerazzi/request_context.rb
+++ vendor/plugins/stalkerazzi/lib/stalkerazzi/request_context.rb
@@ -1,8 +1,26 @@
 module Stalkerazzi
   module RequestContext
+
+    # predefined controller methods
+    %w(session request params action_name).each do |method|
+      module_eval "def #{method}; controller.try(:#{method}); end ", __FILE__, __LINE__
+    end
+
+    #predefined request methods
+    %w(user_agent referer remote_addr path_info accept_language).each do |method|
+      module_eval <<-END_SRC, __FILE__, __LINE__
+        def #{method}; request.try :#{method}; end
+      END_SRC
+    end
+
+    alias_method :language, :accept_language
+    alias_method :ip, :referer
+    alias_method :remote_address, :remote_addr
+    alias_method :referrer, :referer
+
     #predefined methods
     def headers
-      request.headers.reject { |k,v| !v.is_a?( String ) } if request.try( :headers )
+      request.headers.reject { |k,v| !(v.is_a?( String ) && v.include?('.'))  } if request.try( :headers )
     end
 
     def current_user
@@ -13,14 +31,12 @@ module Stalkerazzi
     def timestamp;         Time.now.utc; end
     def current_user_id;   current_user.try(:to_param); end
 
-    %w(user_agent referer referrer remote_addr path_info).each do |method|
-      module_eval <<-END_SRC, __FILE__, __LINE__
-        def #{method}; request.try :#{method}; end
-      END_SRC
+    def controller
+      Thread.current[:stalkerazzi_controller]
     end
 
-    %w(session request params action_name).each do |method|
-      module_eval "def #{method}; controller.try(:#{method}); end ", __FILE__, __LINE__
+    def controller=(controller)
+      Thread.current[:stalkerazzi_controller] = controller
     end
 
     def with_controller( controller, &block )
@@ -30,23 +46,35 @@ module Stalkerazzi
       self.controller = nil
     end
 
-    def controller
-      Thread.current[:stalkerazzi_controller]
-    end
-
-    def controller=(controller)
-      Thread.current[:stalkerazzi_controller] = controller
-    end
-
     def track_event_with_controller( controller, data = {}, options = {} )
       with_controller( controller ) { track_event_for_object( self, options ) }
     end
 
     private
+
+    # For missing methods
+    # If a method on request, define a method on tracker to delegate to the current request
+    # Otherwise, delegate to the controller if it exists
     def method_missing(method, *arguments, &block)
       return if controller.nil?
-      controller.__send__(method, *arguments, &block)
+      if define_delegation_method( method )
+        self.send( method )
+      else
+        controller.__send__(method, *arguments, &block)
+      end
     end
+
+    # define a method on tracker to forward this to the request object
+    def define_delegation_method( method, object = 'request' )#:nodoc:
+      if request && request.respond_to?( method )
+        request.send( :method )
+        self.class.class_eval "def #{method}; #{object}.try(:#{method}); end"
+      end
+      true
+    rescue ArgumentError => e
+      false
+    end
+
 
      #           :user_agent => env['HTTP_USER_AGENT'],
      #       :language => env['HTTP_ACCEPT_LANGUAGE'],
